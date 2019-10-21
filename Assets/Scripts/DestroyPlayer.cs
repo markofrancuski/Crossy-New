d49699477fbbf6d964587b7461d712a645f80566
Stefan Stegic <francmarko@yahoo.com>
2019-10-21 12:33:10 +0200

    Fixed Car Getting blocked

--- Assets/Scripts/DestroyPlayer.cs
+++ Assets/Scripts/DestroyPlayer.cs
@@ -8,6 +8,7 @@ public class DestroyPlayer : MonoBehaviour
     [SerializeField] private string message;
     private void OnCollisionEnter(Collision collision)
     {
+        if(collision.collider.CompareTag("Blockator")) return;
         if (collision.collider.CompareTag("Player") && !GameManager.instance.IsGameOver)
         {
             collision.collider.GetComponent<PlayerController>().PlayDeathAnimation(message);
